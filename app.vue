bd70878962b538e97f6058c2b54fb59821fb6ee7
Maria Rios Navarro <propscode254@gmail.com>
2024-03-29 14:43:25 +0100

    test works

--- app.vue
+++ app.vue
@@ -1,5 +1,23 @@
+<script setup>
+import { createClient } from "@supabase/supabase-js";
+const supabase = createClient(
+  "https://cdtkmwduiolmrwydhfxe.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNkdGttd2R1aW9sbXJ3eWRoZnhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE3MTg0MTQsImV4cCI6MjAyNzI5NDQxNH0.RQMiIVQDlWcT2DtEJtmHwb-jJTqwoO200bwxeXVAWRE"
+);
+const countries = ref([]);
+
+async function getCountries() {
+  const { data } = await supabase.from("countries").select();
+  countries.value = data;
+}
+
+onMounted(() => {
+  getCountries();
+});
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <ul>
+    <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
+  </ul>
 </template>
